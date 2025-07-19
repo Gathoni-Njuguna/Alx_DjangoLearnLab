efa74b55ade6b1a0bd5c30c44c1e4ce5f22fa12c
Gathoni-Njuguna <esternjuguna@gmail.com>
2025-07-19 09:34:03 +0300

    authentication

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -5,8 +5,8 @@ from .views import register_view, login_view, logout_view
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('relationship_app.urls')),
-        # Authentication URLs
-    path('register/', register_view, name='register'),
-    path('login/', login_view, name='login'),
-    path('logout/', logout_view, name='logout'),
+        # Authentication URLs using class-based views
+    path('register/', views.register, name='register'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 ]
