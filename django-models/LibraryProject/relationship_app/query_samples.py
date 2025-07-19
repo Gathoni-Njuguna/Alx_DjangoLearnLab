61a56d682de7a9b8dc7c0bc7749f6864b8406be7
Gathoni-Njuguna <esternjuguna@gmail.com>
2025-07-19 08:38:42 +0300

    implemet changes

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,16 +1,31 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
 def get_books_by_author(author_name):
-    """Query all books by a specific author using filter()"""
-    return Book.objects.filter(author__name=author_name)
+    """
+    Query all books by a specific author
+    """
+    try:
+        author = Author.objects.get(name=author_name)
+        return author.books.all()  # Using the related_name 'books'
+    except Author.DoesNotExist:
+        return Book.objects.none()
 
 def get_books_in_library(library_name):
-    """List all books in a library using filter()"""
-    return Book.objects.filter(libraries__name=library_name)
+    """
+    List all books in a library
+    """
+    try:
+        library = Library.objects.get(name=library_name)
+        return library.books.all()  # ManyToMany relationship
+    except Library.DoesNotExist:
+        return Book.objects.none()
 
 def get_librarian_for_library(library_name):
-    """Retrieve the librarian for a library using filter()"""
+    """
+    Retrieve the librarian for a library
+    """
     try:
-        return Librarian.objects.get(library__name=library_name)
-    except Librarian.DoesNotExist:
+        library = Library.objects.get(name=library_name)
+        return library.librarian  # OneToOne relationship (using related_name)
+    except Library.DoesNotExist:
         return None
