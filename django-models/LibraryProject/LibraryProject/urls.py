b3ae789ebf34dd95595006131e72fac393b2a5e8
Gathoni-Njuguna <esternjuguna@gmail.com>
2025-07-19 09:36:43 +0300

    authentication

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import register_view, login_view, logout_view
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
