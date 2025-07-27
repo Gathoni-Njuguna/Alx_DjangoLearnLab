12afce479ced2cb77ef2d86031595fac375a983c
Gathoni-Njuguna <esternjuguna@gmail.com>
2025-07-27 17:28:36 +0300

    done

---
+++ advanced_features_and_security/LibraryProject/LibraryProject/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.contrib.auth.views import LoginView, LogoutView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('relationship_app.urls')),
+        # Authentication URLs using class-based views
+    path('register/', views.register, name='register'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+]
