29d9e757abb137251d8d4480a55595ced290ee90
Gathoni-Njuguna <esternjuguna@gmail.com>
2025-07-27 17:45:09 +0300

    done

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -3,7 +3,12 @@ from django.db import models
 from django.contrib.auth.models import Group, Permission
 from django.contrib.contenttypes.models import ContentType
 from django.utils.translation import gettext_lazy as _
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
 
+    def __str__(self):
+        return self.title
 class CustomUserManager(BaseUserManager):
     """Define a model manager for User model with no username field."""
 
