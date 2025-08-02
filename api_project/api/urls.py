ab1d36cce6c08c1ccd20387ff04151975b1ef8da
Gathoni-Njuguna <esternjuguna@gmail.com>
2025-08-02 08:15:48 +0300

    serializer

---
+++ api_project/api/urls.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from django.urls import path, include
+from api.views import BookList
+urlpatterns = [
+    path('books/', BookList.as_view(), name='book-list'),  # Maps to the BookList view
+]
