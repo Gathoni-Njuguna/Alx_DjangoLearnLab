0ff424d56f3c347b521e5fea128b194057533fc3
Gathoni-Njuguna <esternjuguna@gmail.com>
2025-08-08 12:43:11 +0300

    crud

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -5,6 +5,7 @@ from .serializers import BookSerializer
 from rest_framework import permissions
 from django.http import HttpResponse
 from .permissions import IsCreatorOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 
 # Create your views here.
