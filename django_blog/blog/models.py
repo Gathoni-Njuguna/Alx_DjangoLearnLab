e733d0fd194532315651df3ee1c998398810757f
Gathoni-Njuguna <esternjuguna@gmail.com>
2025-08-13 10:13:15 +0300

    new

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-from django.conf import settings
+from django.contrib.auth.models import User
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(        settings.AUTH_USER_MODEL,       # Reference to Django's user model
+    author = models.ForeignKey(       User,       # Reference to Django's user model
         on_delete=models.CASCADE,        # Required: specify delete behavior
         related_name='posts')
