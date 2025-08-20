301cd0a68218283b794f5d884c7021adddfa1326
Gathoni-Njuguna <esternjuguna@gmail.com>
2025-08-20 10:24:33 +0300

    serializer and model

---
+++ social_media_api/accounts/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser, Group, Permission
+
+class CustomUser(AbstractUser):
+    bio = models.TextField(blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+    followers = models.ManyToManyField('self', symmetrical=False, related_name='following', blank=True)
+    groups = models.ManyToManyField(
+        Group,
+        related_name='customuser_set',  # unique reverse name
+        blank=True,
+        help_text='The groups this user belongs to.',
+        verbose_name='groups'
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name='customuser_set',  # unique reverse name
+        blank=True,
+        help_text='Specific permissions for this user.',
+        verbose_name='user permissions'
+    )
+    def __str__(self):
+        return self.name
+    
+# Create your models here.
